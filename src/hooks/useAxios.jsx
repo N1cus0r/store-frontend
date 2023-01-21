0d208931d1a3a4f9dc3c9cdea3a7990282cad9fe
nicu <lino479Nick@mail.ru>
2023-01-22 00:23:09 +0300

    Edited Axios Instance

--- src/hooks/useAxios.jsx
+++ src/hooks/useAxios.jsx
@@ -2,7 +2,8 @@ import axios from "axios";
 
 const useAxios = () => {
   const axiosInstance = axios.create({
-    baseURL: "http://127.0.0.1:8000/",
+    // baseURL: "http://127.0.0.1:8000/",
+    baseURL: "https://store-frontend.onrender.com/",
     headers: {
       "Content-Type": "application/json",
     },
